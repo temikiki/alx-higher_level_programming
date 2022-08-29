b21564c9e8f56caed85ae1e65552eb2a341a4638
Roy Eugene <71348610+roy-eugene049@users.noreply.github.com>
2022-08-29 15:38:33 +0300

    Initial Commit

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,1 +1,10 @@
-lets go
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    """Print the addition of all the arguments."""
+    import sys
+
+    total = 0
+    for i in range(len(sys.argv) - 1):
+        total += int(sys.argv[i + 1])
+    print("{}".format(total))
