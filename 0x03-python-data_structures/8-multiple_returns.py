21a2460a3148b6913876f61241764bab29abe781
Roy Eugene <71348610+roy-eugene049@users.noreply.github.com>
2022-08-29 17:31:30 +0300

    Update 8-multiple_returns.py

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,1 +1,7 @@
-ll
+#!/usr/bin/python3
+
+def multiple_returns(sentence):
+    """Returns the length of a string and its first character."""
+    if sentence == "":
+        return (0, None)
+    return (len(sentence), sentence[0])
