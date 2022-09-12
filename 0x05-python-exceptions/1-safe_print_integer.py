64a86f3d47297cdd71df042edc6ab9d234c8c417
Roy Eugene <71348610+roy-eugene049@users.noreply.github.com>
2022-09-12 18:06:55 +0300

    Initial Commit

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,1 +1,9 @@
-ll
+#!/usr/bin/python3
+
+
+def safe_print_integer(value):
+    try:
+        print("{:d}".format(value))
+        return (True)
+    except (TypeError, ValueError):
+        return (False)
