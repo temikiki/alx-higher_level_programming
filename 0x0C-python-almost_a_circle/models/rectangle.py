1ecfd8cd167c4a0c95b7fe94a1b4a37f4eb38b84
temikiki <temitayokiki@gmail.com>
2022-10-07 03:59:11 -0700

    r

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,12 +1,16 @@
 #!/usr/bin/python3
-"""
-initialisation of module
-"""
+"""module initialisation"""
 
-from base import Base
+
+from models.base import Base
+# from base import Base
+
 
 class Rectangle(Base):
+    """this rectangle inherits fr0m class Base"""
+
     def __init__(self, width, height, x=0, y=0, id=None):
+        """initialisation of attributes"""
         self.width = width
         self.height = height
         self.x = x
@@ -15,50 +19,76 @@ class Rectangle(Base):
 
     @property
     def width(self):
-        """get value of width"""
-        return self._width
-    
-    @width.setter
-    def width(self, Value):
-        """set width, raise error if negative"""
-        if Value >= 0:
-            self._width = Value
-        else:
-            raise ValueError("width must be positive")
+        return self.__width
 
     @property
     def height(self):
-        """get value of height"""
-        return self._height
-    
-    @height.setter
-    def height(self, Value):
-        """ set value for height"""
-        if Value >= 0:
-            self._height = Value
-        else:
-            raise ValueError("height must be postive")
+        return self.__height
+
     @property
     def x(self):
-        """get the value of x"""
-        return self._x
-    @x.setter
-    def x(self, Value):
-        """set the value of x"""
-        if Value >= 0:
-            self._x = Value
-        
-        else:
-            raise ValueError("x must be postive")
+        return self.__x
+
     @property
     def y(self):
-        """get the value of y"""
-        return self.y 
-    @y.setter
-    def y(self, Value):
-        """set value of y"""
-        if value >= 0:
-            self._y = Value
-        else:
-            raise ValueError("y must be positive")
+        return self.__y
 
+    @width.setter
+    def width(self, value):
+        """sets the width"""
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value <= 0:
+            raise ValueError("width must be > 0")
+
+        self.__width = value
+
+    @height.setter
+    def height(self, value):
+        """sets the height"""
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        if value <= 0:
+            raise ValueError("height must be > 0")
+
+        self.__height = value
+
+    @x.setter
+    def x(self, value):
+        """sets x as value"""
+        if type(value) is not int:
+            raise TypeError("x must be an integer")
+        if value < 0:
+            raise ValueError("x must be >= 0")
+
+        self.__x = value
+
+    @y.setter
+    def y(self, value):
+        """sets y as value"""
+        if type(value) is not int:
+            raise TypeError("y must be an integer")
+        if value < 0:
+            raise ValueError("y must be >= 0")
+
+        self.__y = value
+
+    def area(self):
+        """returns the area of rectangle"""
+        return self.width * self.height
+
+    def display(self):
+        """displays the recangle"""
+        for y in range(self.__y):
+            print("")
+        for i in range(self.__height):
+            for x in range(self.x):
+                print(" ", end="")
+            for j in range(self.__width):
+                print("#", end="")
+            print()
+
+    def __str__(self):
+        """overrides the class str method"""
+        return f"[Rectangle] ({self.id}) {self.x}/{self.y} -\
+                {self.width}/{self.height}"
